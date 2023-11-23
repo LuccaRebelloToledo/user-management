1f6665487be5bdb856845b9bf1d8319b3b675dcd
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2023-11-23 16:42:01 -0300

    feat: migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,13 +4,13 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  username: process.env.PG_USER,
+  password: process.env.PG_PASS,
+  database: process.env.PG_DB,
   entities: [
-    `${__dirname}/**/modules/infra/typeorm/entities/*.{ts, js}`
+    `${__dirname}/**/modules/**/infra/typeorm/entities/*.{ts, js}`
   ],
   migrations: [
     `${__dirname}/**/shared/infra/http/typeorm/migrations/*.{ts, js}`
